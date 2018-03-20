dd7c3f6b4e3f52265d69baa8118b50df43906fc9
ies <ies@c3-8>
2018-03-20 21:17:30 +0100

    new tablero for battles

--- src/tablero.java
+++ src/tablero.java
@@ -11,6 +11,32 @@ public class tablero {
 				{ 1, 0, 0, 0, 0, 0, 0, 0, 1},
 				{ 8, 1, 0, 0, 0, 0, 0, 0, 1},
 		};
+		
+		public static int battletable[][]= {
+				
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, //1
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,7,7,7,7,7,1}, //2
+				{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,9,1,1,1,1,1,1,1,1,6,1,1,7,7,7,7,7,1}, //3
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,15,1,1,1,1,1,1,1,1,1,1,1,7,7,7,7,7,1}, //4
+				{1,7,7,7,7,7,1,1,1,1,1,1,1,1,1,1,1,15,1,4,4,4,4,4,4,1,5,1,1,7,7,7,7,7,1}, //5
+				{1,7,7,7,7,7,1,1,6,1,1,1,1,1,1,1,1,15,1,4,4,4,4,4,4,1,1,1,1,7,7,7,7,7,1}, //6
+				{1,7,7,7,7,7,1,1,1,1,1,1,1,1,1,1,1,15,1,1,1,1,1,1,1,1,1,1,1,8,8,8,8,8,1}, //7
+				{1,7,7,7,7,7,1,1,5,1,4,4,4,4,4,4,1,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, //8
+				{1,7,7,7,7,7,1,1,1,1,4,4,4,4,4,4,1,10,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}, //9
+				{1,8,8,8,8,8,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, //10
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, //11
+				{1,11,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,13,1}, //12
+				{1,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,16,1}, //13
+				{1,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,16,1}, //14
+				{1,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,16,1}, //15
+				{1,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,16,1}, //16
+				{1,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,16,1}, //17
+				{1,16,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,16,1}, //18
+				{1,12,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,14,1}, //19
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, //20
+				
+		};
+		
 	public static void main(String[] args) {
 	
 		// TODO Auto-generated method stub
@@ -19,39 +45,42 @@ public class tablero {
 	}
 	
     private static void inicialitzarGUI() {
-    	t.setActimatges(true); /**  @param actimatges activo imatges
-    							*/
-        t.setActimgbackground(true);/**  @param actimgbackground activo background
-		*/
-        t.setImgbackground("fondoprueba2.jpg");
+    	t.setActimatges(true);  // /**  @param actimatges activo imatges
+    	t.setActimgbackground(false);	/*/
+       // t.setActimgbackground(true);/**  @param actimgbackground activo background
+									//*/
+       // t.setImgbackground("fondoprueba2.jpg");
         /**  @param setimgbackground Declaro el path del background
 		*/
     	String[] imatges= {"" //0 empty
-    			,"dio.jpg" //1 Dio
-    			,"","","","","","", //2-7 empty
-    			"mimikyu.png", //8 mimikyu
-    			"Shaman.png", //9 shaman hearthstone
-    			"", //10 empty
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg", "knuckles.jpg",
-    			"knuckles.jpg"//11-72 Uganda knuckles
-    			}; 
+    			,"" //1 Empty
+    			,"separador.jpg" //2 Separador horizontal verde
+    			,"separador_menu.jpg" //3 Separador horizontal naranja
+    			,"peloticavacia.jpg" //4 Redonda vacia
+    			,"HP.jpg" //5
+    			,"" //6 empty
+    			,"entrenador.jpg", //7 entrenador
+    			"rival.jpg", //8 ira el nombre
+    			"up_down.jpg", //9 curva de izq-abajo verde
+    			"down_up.jpg", //10 curva de arriba-der verde
+    			"left_right.jpg", //11
+    			"down_up_menu.jpg", //12
+    			"up_down_menu.jpg", //13
+    			"right_left.jpg", //14
+    			"vertical.jpg", //15 Separador vertical verde
+    			"vertical_menu+.jpg" //16 separador vertical naranja
+    			};
     	t.setImatges(imatges); //Paso la string de imatges
+    	
         t.setActcolors(false); //Desactivo els colors
-        t.setPAD(0); //Posso el PAD a 0
-        f.setActetiquetes(false); //Desactivo etiquetes
+       
 	}
 	
 	public static void viewtable() {
 		/*
 		 * Funcio que dibuixa el tauler
 		 */
-		t.dibuixa(table);
+		t.dibuixa(battletable);
 	}
 	
 	public static void ready() {
